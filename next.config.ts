f69c600ac9688e7a3bff4f9c92edd157d1dd5915
[200~ user.name <ronaldowagner2011@hotmail.com>
2026-06-19 17:18:57 -0300

    fix: remove standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  serverExternalPackages: ['next-auth'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 }
 export default nextConfig
