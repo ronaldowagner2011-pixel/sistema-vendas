13ea17bfa29b55e9b811311f8d832da3b82647f5
[200~ user.name <ronaldowagner2011@hotmail.com>
2026-06-19 16:59:30 -0300

    fix: volta nextauth v4 e corrige config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: ['next-auth'],
-  },
+  serverExternalPackages: ['next-auth'],
 }
 export default nextConfig
