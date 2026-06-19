7f48afad4b963c30ed51b33134b4283b219182a0
[200~ user.name <ronaldowagner2011@hotmail.com>
2026-06-19 16:22:04 -0300

    fix: simplifica postcss config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,1 +1,8 @@
-const config = { plugins: { "@tailwindcss/postcss": {}, autoprefixer: {}, }, }; export default config; 
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+export default config;
